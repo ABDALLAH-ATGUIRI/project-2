3aa254bba757b1ee6173e6f9434791835368f347
ABDALLAH-ATGUIRI <94124850+ABDALLAH-ATGUIRI@users.noreply.github.com>
2021-11-14 15:49:33 +0100

    Update PROJECT-11.c

--- PROJECT-11.c
+++ PROJECT-11.c
@@ -10,49 +10,43 @@ int main()
 	scanf("%d",&A);
 	printf("Enter the process = \n");
 	scanf("%s",&X);
-//	printf("Enter la valeur B = \n");
-//	scanf("%d",&B);
-//	for(j=1;j<=5;j++){
 
-    //switch (j){
-    	//case 1:{
+		//for(i=1;i<=5;i++){
+			switch (X){
+				case '+':{
 
-		for(i=1;i<=5;i++){
-			switch (i){
-				case 1:{
-				if (X=='+' ){
 				printf("Enter la valeur B = \n");
 				scanf("%d",&B);
 			C=A+B;
 			printf("Total %d %c %d = %d\n",A,X,B,C);
-			}
+
 			break;
 		}
-			case 2:{
-			if(X=='-'){
+			case '-':{
+
 				printf("Enter la valeur B = \n");
 				scanf("%d",&B);
 			C=A-B;
 			printf("Total %d %c %d = %d\n",A,X,B,C);
-			}
+
 			break;
 		}
-			case 3:{
-			if(X=='*'){
+			case '*':{
+
 				printf("Enter la valeur B = \n");
 				scanf("%d",&B);
 		    C=A*B;
 			printf("Total %d %c %d = %d\n",A,X,B,C);
-		}	
+
 			break;
 		}
-			case 4:{
-			if(X=='/'){
+			case '/':{
+
 				do {	printf("Enter la valeur B = \n");
 					scanf("%d",&B);
-		
+
 			if(B!=0){
-			
+
 			C=A/B;
 			printf("Total %d %c %d = %d\n",A,X,B,C);
 			B-=B;}
@@ -60,34 +54,38 @@ int main()
 				printf("Can not be divided at zero tried again : \n");
 			}
 			}while(B==0);
-			
-		}	
+
+
 			break;
 		}
-		case 5:{
-			if(X=='^'){
+		case '^':{
+
 				printf("Entre type the power : \n");
-				scanf("%d",P);
-			
+				scanf("%d",&P);
+				C=1;
 				for (i=0;i<=P;i++){
-					A=A*A;
-				
+				C*=A;
+
 				}
-				printf("Solution :%d\n",C=+A );
-			}
-			
+				printf("Solution :%d\n",C );
+
 			break;
 		}
-		
-		
+		case '!':{
+       printf("hi");
+      
+		break;
+		}
+      
+
 	}
-	}
+
 
 		//break;
 //		}
 //		case 2:{
-//			
-//			
+//
+//
 //			break;
 //		}
 //}
